5b1a9679479a6f835cc2c36a3f5cadec1136cf43
IGambitI <baranowski.filip13@wp.pl>
2024-06-06 14:37:51 +0200

    COMMENT-1 Added comments to skills section component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,24 +8,11 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
+  /*Tilt definition for tilting cards from react-tilt*/
   <Tilt className='xs:w-[250px] w-full'>
-    <motion.div
-      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
-    >
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
-      >
-        <img
-          src={icon}
-          alt='web-development'
-          className='w-16 h-16 object-contain'
-        />
+    <motion.div variants={fadeIn('right', 'spring', index * 0.5, 0.75)} className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'>
+      <div options={{max: 45, scale: 1, speed: 450,}} className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
+        <img src={icon} alt='web-development' className='w-16 h-16 object-contain'/>
 
         <h3 className='text-white text-[20px] font-bold text-center'>
           {title}
@@ -33,20 +20,19 @@ const ServiceCard = ({ index, title, icon }) => (
       </div>
     </motion.div>
   </Tilt>
-);
+)
 
 const Skills = () => {
   return (
     <>
+      {/*Motion div for headers to add motion on load*/}
       <motion.div variants={textVariant()}>
         <p className={style.sectionSubText}>Introduction</p>
         <h2 className={style.sectionHeadText}>Overview.</h2>
       </motion.div>
 
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
-      >
+      {/*Motion p for text to add motion on load*/}
+      <motion.p variants={fadeIn("", "", 0.1, 1)} className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
         I'm a skilled software developer with experience in TypeScript and
         JavaScript, and expertise in frameworks like React, Node.js, and
         Three.js. I'm a quick learner and collaborate closely with clients to
@@ -54,13 +40,14 @@ const Skills = () => {
         real-world problems. Let's work together to bring your ideas to life!
       </motion.p>
 
+      {/*Cards that are to be tilted*/}
       <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
     </>
-  );
-};
+  )
+}
 
 export default SectionWrapper(Skills, "skills");
